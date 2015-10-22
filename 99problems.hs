a7756e305c0a53e4f6555b4723d3ada85f2bd24a
arturbmallmann <arturbmallmann@gmail.com>
2015-10-22 16:25:13 -0200

    .

--- 99problems.hs
+++ 99problems.hs
@@ -1,7 +1,14 @@
+-- 1..10 list
+{-
+ - ex. 1
+ -}
 ultimo (x:[]) = x
 ultimo (x:xs) = ultimo xs
 
 --penultimo :: [a] -> Maybe a
 --penultimo (x:[]) = Nothing 
+{-
+ - ex. 2
+ -}
 penultimo (x:_:[]) = x
 penultimo (x:xs) = penultimo xs 
